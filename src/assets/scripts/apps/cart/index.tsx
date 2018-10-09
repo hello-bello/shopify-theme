96dc3f3f6edb69207f2ad0f88fae4e944abdb93b
Ethan Jon <e10jc@users.noreply.github.com>
2018-10-09 07:50:18 -0700

    simple cart functionality

--- src/assets/scripts/apps/cart/index.tsx
+++ src/assets/scripts/apps/cart/index.tsx
@@ -3,21 +3,64 @@ import * as ReactDOM from 'react-dom'
 
 interface Props {}
 
-class CartApp extends React.Component<Props> {
+class CartApp extends React.Component<Props, Cart> {
   componentDidMount () {
     window.CartApp = {
-      addToCart: this.addToCart
+      addToCart: this.addToCart,
+      removeFromCart: this.removeFromCart,
+    }
+    this.setState(window.cart)
+  }
+
+  addToCart = async (variantId: string): Promise<boolean> => {
+    const body = new FormData()
+    body.append('id', variantId)
+    body.append('quantity', '1')
+
+    try {
+      await fetch('/cart/add.js', {
+        body,
+        method: 'POST',
+      })
+      await this.getCart()
+      return true
+    } catch (e) {
+      return false
     }
   }
 
-  addToCart = (variantId: string): boolean => {
-    console.log(`adding variant id ${variantId} to cart`)
-    return true
+  getCart = async () => {
+    const res = await fetch('/cart.js')
+    const cart = await res.json()
+    this.setState(cart)
+  }
+
+  removeFromCart = async (item: Item): Promise<boolean> => {
+    const body = new FormData()
+    body.append('id', item.id.toString())
+    body.append('quantity', '0')
+
+    try {
+      await fetch('/cart/change.js', {
+        body,
+        method: 'POST',
+      })
+      await this.getCart()
+      return true
+    } catch (e) {
+      return false
+    }
   }
 
   render () {
+    if (!this.state) return null
+
     return <div>
       <h2>Cart</h2>
+      {this.state.items.map(item => <div key={item.id}>
+        <span>{item.title}</span>
+        <button className='button' onClick={() => this.removeFromCart(item)}>Remove</button>
+      </div>)}
     </div>
   }
 }
--- src/assets/scripts/apps/cart/typings.d.ts
+++ src/assets/scripts/apps/cart/typings.d.ts
@@ -1,5 +1,16 @@
+interface Cart {
+  items: Item[],
+}
+
+interface Item {
+  id: number,
+  title: string,
+}
+
 interface Window {
+  cart: Cart,
   CartApp: {
-    addToCart: (string) => boolean
+    addToCart: (string) => Promise<boolean>,
+    removeFromCart: (Item) => Promise<boolean>,
   }
 }
