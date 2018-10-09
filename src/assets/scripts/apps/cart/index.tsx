d2c29c952d3737447dc2627c59d5eebc9b82c565
Ethan Jon <e10jc@users.noreply.github.com>
2018-10-09 08:29:45 -0700

    added tslint

--- src/assets/scripts/apps/cart/index.tsx
+++ src/assets/scripts/apps/cart/index.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-interface Props {}
-
-class CartApp extends React.Component<Props, Cart> {
-  componentDidMount () {
+class CartApp extends React.Component<{}, Cart> {
+  public componentDidMount() {
     window.CartApp = {
       addToCart: this.addToCart,
       removeFromCart: this.removeFromCart,
@@ -12,7 +10,19 @@ class CartApp extends React.Component<Props, Cart> {
     this.setState(window.cart)
   }
 
-  addToCart = async (variantId: string): Promise<boolean> => {
+  public render() {
+    if (!this.state) { return null }
+
+    return <div>
+      <h2>Cart</h2>
+      {this.state.items.map(item => <div key={item.id}>
+        <span>{item.title}</span>
+        <button className='button' onClick={() => this.removeFromCart(item)}>Remove</button>
+      </div>)}
+    </div>
+  }
+
+  private addToCart = async (variantId: string): Promise<boolean> => {
     const body = new FormData()
     body.append('id', variantId)
     body.append('quantity', '1')
@@ -29,13 +39,13 @@ class CartApp extends React.Component<Props, Cart> {
     }
   }
 
-  getCart = async () => {
+  private getCart = async () => {
     const res = await fetch('/cart.js')
     const cart = await res.json()
     this.setState(cart)
   }
 
-  removeFromCart = async (item: Item): Promise<boolean> => {
+  private removeFromCart = async (item: Item): Promise<boolean> => {
     const body = new FormData()
     body.append('id', item.id.toString())
     body.append('quantity', '0')
@@ -51,19 +61,7 @@ class CartApp extends React.Component<Props, Cart> {
       return false
     }
   }
-
-  render () {
-    if (!this.state) return null
-
-    return <div>
-      <h2>Cart</h2>
-      {this.state.items.map(item => <div key={item.id}>
-        <span>{item.title}</span>
-        <button className='button' onClick={() => this.removeFromCart(item)}>Remove</button>
-      </div>)}
-    </div>
-  }
 }
 
 const el = document.getElementById('cart-app')
-if (el) ReactDOM.render(<CartApp />, el)
+if (el) { ReactDOM.render(<CartApp />, el) }
