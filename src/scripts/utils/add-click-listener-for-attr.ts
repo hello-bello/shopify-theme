36328cf38125c79e724ff3ab01191ca7f512c5d8
Ethan Jon <e10jc@users.noreply.github.com>
2018-11-06 16:17:49 -0800

    cart button

--- src/scripts/utils/add-click-listener-for-attr.ts
+++ src/scripts/utils/add-click-listener-for-attr.ts
@@ -2,9 +2,13 @@ export default (attr: string, fn: (val: string, target: HTMLElement) => void) =>
   document.body.addEventListener('click', (e) => {
     if (e.target instanceof HTMLElement) {
       const target = e.target as HTMLElement
-      if (target.hasAttribute(attr)) {
+
+      // only <a> tags can have the attribute, and we have to look up the DOM to handle cases like <a data-whatever><img></a>
+      const aTarget = target.tagName === 'A' ? target : target.closest('a')
+
+      if (aTarget && aTarget.hasAttribute(attr)) {
         e.preventDefault()
-        fn(target.getAttribute(attr) || '', target)
+        fn(aTarget.getAttribute(attr) || '', aTarget)
       }
     }
   })
