448223b8a9f2c4251a33ef6811873052f2e95c51
Ethan Jon <e10jc@users.noreply.github.com>
2018-11-12 08:39:27 -0800

    bundler-app

---
+++ src/scripts/apps/bundler/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+
+import renderApp from '../../utils/render-app'
+import BundlerApp from './app'
+
+renderApp(<BundlerApp />, 'bundler-app')
--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -1,6 +1,7 @@
 import '../../styles/layout/theme.scss'
 
 import '../i18n/'
+import '../apps/bundler/'
 import '../apps/cart/'
 import '../plugins/accordion-menu/'
 import '../plugins/drawer/'
